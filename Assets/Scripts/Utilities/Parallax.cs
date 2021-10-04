7f1ed7d0873938df06a64aa43f74733ca0fc3267
Khami-Desktop <napakamee@outlook.com>
2021-10-04 20:25:18 +0700

    Add repeatable cloud

--- Assets/Scripts/Utilities/Parallax.cs
+++ Assets/Scripts/Utilities/Parallax.cs
@@ -5,31 +5,38 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     private float length, startpos;
+    [SerializeField] private bool repeat;
     [SerializeField] private float offset;
-    [SerializeField] private GameObject cam;
+    [SerializeField] private Camera cam;
     [SerializeField] private float parallaxEffect;
     
+    private float lowerCam, upperCam;
     void Start()
     {
         startpos = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.y;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float _temp = cam.transform.position.y * (1 - parallaxEffect);
+        float _pos = transform.position.y;
         float _dist = ((cam.transform.position.y / 2) * parallaxEffect) + offset;
 
         transform.position = new Vector3(transform.position.x, startpos + _dist, transform.position.z);
 
-        // if (_temp > startpos + length)
-        // {
-        //     startpos += length;
-        // }
-        // else if (_temp < startpos - length)
-        // {
-        //     startpos -= length;
-        // }
+        if (repeat)
+        {
+            lowerCam = cam.ViewportToWorldPoint(new Vector3(0, cam.rect.min.y)).y;
+            upperCam = cam.ViewportToWorldPoint(new Vector3(0, cam.rect.max.y)).y;
+        
+            if (_pos <= lowerCam - 10)
+            {
+                var _newPos = Random.Range(0, 3f);
+                startpos = upperCam + _newPos;
+            }
+        }
+        
     }
 }
